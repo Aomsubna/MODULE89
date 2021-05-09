c1107224bb2756cc10b3a1e65174db08b36d199b
Aomsubna <56284463+Aomsubna@users.noreply.github.com>
2021-05-09 21:10:18 +0700

    aomsub trajec

--- Core/Inc/TRAJECTORY.h
+++ Core/Inc/TRAJECTORY.h
@@ -14,8 +14,22 @@ typedef struct{
 	//number
 	int number;
 
+	//x, y trajectory
+	volatile float Xt, Yt;
+
+	//vx vy trajectory
+	volatile float Vxt, Vyt;
+
+	//constant c2 c3
+	volatile float C2, C3;
+
 
 }trajectory_state;
 
+void setuptrajectory(float xt, float yt, float vxt, float vyt, float c2, float c3);
+
+float calculationsetup(float x_pos, float y_pos,float r, float ome, float x_en, float y_en);
+
+float calculationtrajectory(float tau,float theta,float x_posnew, float y_posnew);
 
 #endif /* __TRAJECTORY_H */
--- Core/Src/TRAJECTORY.c
+++ Core/Src/TRAJECTORY.c
@@ -4,6 +4,42 @@
 // Group name: ISUS
 
 #include "TRAJECTORY.h"
+#include <math.h>
 
 static trajectory_state trajectorys[NUM_TRAJECTORY];
 
+void setuptrajectory(float xt, float yt, float vxt, float vyt, float c2, float c3){
+	trajectory_state * trajectory = &trajectorys[NUM_TRAJECTORY];
+	trajectory -> Xt = xt;
+	trajectory -> Yt = yt;
+	trajectory -> Vxt = vxt;
+	trajectory -> Vyt = vyt;
+	trajectory -> C2 = c2;
+	trajectory -> C3 = c3;
+}
+
+
+float calculationsetup(float x_pos, float y_pos,float r, float ome, float x_en, float y_en){
+	trajectory_state * trajectory = &trajectorys[NUM_TRAJECTORY];
+	float tau = sqrt(pow((x_pos - x_en),2.0) + pow((y_pos - y_en),2.0));
+	float theta = ome * tau;
+	float x_posnew = x_pos + r*cos(theta);
+	float y_posnew = y_pos + r*sin(theta);
+
+	trajectory -> C2 = (3.0*(sqrt(pow((x_posnew - x_en),2.0) + pow((y_posnew - y_en),2.0))))/(pow(tau,2.0));
+	trajectory -> C3 = ((-2.0)*(sqrt(pow((x_posnew - x_en),2.0) + pow((y_posnew - y_en),2.0))))/(pow(tau,3.0));
+
+	return trajectory -> C2,trajectory -> C3, tau, x_posnew, y_posnew, theta;
+}
+
+
+float calculationtrajectory(float tau,float theta,float x_posnew, float y_posnew){
+	trajectory_state * trajectory = &trajectorys[NUM_TRAJECTORY];
+	float t;
+	while(t <= tau){
+		t += 1;
+		float rt = (trajectory -> C2 * t *t) + (trajectory -> C3 *t*t*t);
+		trajectory -> Xt = x_posnew + rt*cos(theta);
+		trajectory -> Yt = y_posnew + rt*sin(theta);
+	}
+}
